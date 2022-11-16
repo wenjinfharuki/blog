24dcda236723701efd5ccd96f2132600bd4c45ff
wenjingharuki <112315722+wenjinfharuki@users.noreply.github.com>
2022-11-16 13:37:18 +0900

    Add files via upload

---
+++ renwenjing/RegisterController.java
@@ -0,0 +1,35 @@
+package renwenjing.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import renwenjing.example.model.UserService;
+
+@Controller
+public class RegisterController {
+	@Autowired
+	private UserService userService;
+
+	// 新規登録画面を表示
+	@GetMapping("/register")
+	public String getRegisterPage() {
+		return "register.html";
+	}
+
+	// ユーザー情報の登録
+	@PostMapping("/register")
+	public String register(@RequestParam String username, @RequestParam String password, Model model) {
+		// もし保存をした場合には、newAccount.htmlへ遷移する
+		if (userService.createAccount(username, password)) {
+			return "login.html";
+		} else {
+			// そうでない場合には、register.htmlに遷移する
+			return "register.html";
+
+		}
+	}
+}
